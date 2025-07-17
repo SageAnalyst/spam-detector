e3bd0c7eebb867d51748f0f034d1168af37ebabd
jamiiu <clean@Jamius-MacBook-Pro.local>
2025-07-17 16:41:36 +0100

    Fix Logging function

--- spmdetctweb.py
+++ spmdetctweb.py
@@ -12,9 +12,9 @@ from nltk.corpus import stopwords
 nltk.download("punkt")
 
 # Load models
-model = joblib.load("spam_detection_model_compressed.pkl")
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-scaler = joblib.load("scaler.pkl")
+model = joblib.load("/Users/clean/spam-detector/spam_detection_model_compressed.pkl")
+vectorizer = joblib.load("/Users/clean/spam-detector/tfidf_vectorizer.pkl")
+scaler = joblib.load("/Users/clean/spam-detector/scaler.pkl")
 
 # Preprocessing
 ps = PorterStemmer()
@@ -24,6 +24,14 @@ def preprocess(text):
     text_nostop = [word.lower() for word in tokens if word not in stopwords.words('english')]
     stemmed = [ps.stem(word) for word in text_nostop]
     return " ".join(stemmed)
+
+#LOGGING FUNCTION
+import datetime
+
+def log_user_input(user_input, prediction):
+    with open("user_logs.txt", "a") as log_file:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_file.write(f"{timestamp} | Input: {user_input[:300]} | Prediction: {prediction}\n")
 
 # USER INTERFACE
 st.title("JAMIU'S SPAM DETECTION MODEL")
@@ -47,3 +55,6 @@ if st.button("Predict"):
     prediction = model.predict(features)[0]
     label = "📬 Ham" if prediction == 0 else "🚫 Spam"
     st.success(f"This message is: {label}")
+
+     # 📝 Log input and result
+    log_user_input(user_input, "Spam" if prediction == 1 else "Ham")
